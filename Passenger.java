239f1b74cae3ede9820d587264c1feece939f38d
vasantikale82-boop <vasantikale82@gmail.com>
2026-01-08 16:06:28 +0530

    Create Passenger.java

---
+++ Passenger.java
@@ -0,0 +1,17 @@
+public class Passenger {
+    private String name;
+    private String trainNumber;
+
+    public Passenger(String name, String trainNumber) {
+        this.name = name;
+        this.trainNumber = trainNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTrainNumber() {
+        return trainNumber;
+    }
+}
